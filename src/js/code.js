5455ca1ea5b4c9f130a404841863f0603573e00e
Lina7227 <78968726+Lina7227@users.noreply.github.com>
2021-05-01 19:40:19 +0300

    adaptiv

--- src/js/code.js
+++ src/js/code.js
@@ -10,3 +10,14 @@ var swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
   });
+  
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".nav_mobile");
+burger.onclick = () => {
+    if (nav.style.display == "flex") {
+        nav.style.display = "none";
+    } 
+    else {
+        nav.style.display = "flex";
+    }
+};
